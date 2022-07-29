064880377c7710eb9475975ca52795a1341abe98
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 11:33:40 +0900

    追加バリデーションのテストコードの記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe User, type: :model do
       end
     end
     context '新規登録できない場合' do
+
+      #入力欄が空の場合のバリデーション検証
+
       it "nicknameが空では登録できない" do
         @user.nickname = ''
         @user.valid?
@@ -51,6 +54,70 @@ RSpec.describe User, type: :model do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+
+      #emailのバリデーション検証
+
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+     it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
+      #passwordのバリデーション検証
+
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = 'ya290'
+        @user.password_confirmation = 'ya290'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordが数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password  is invalid. Include both letters and numbers')
+      end
+      it 'passwordが英語のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password  is invalid. Include both letters and numbers')
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = 'yamada290'
+        @user.password_confirmation = 'tarou290'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      #本人情報のバリデーション検証
+
+      it 'last_nameは全角全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+        @user.last_name = 'ｱｲｳｴｵ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name  is invalid. Input full-width characters")
+      end
+      it 'last_nameは全角全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
+        @user.first_name = 'ｱｲｳｴｵ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name  is invalid. Input full-width characters")
+      end
+      it 'last_name_kanaは全角全角（カタカナ）でなければ登録できない' do
+        @user.last_name_kana = 'あいうえお'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana  kana is invalid. Input full-width katakana characters")
+      end
+      it 'last_nameは全角全角（カタカナ）でなければ登録できない' do
+        @user.first_name_kana = 'あいうえお'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana  kana is invalid. Input full-width katakana characters")
       end
 
     end
