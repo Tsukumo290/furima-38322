d9ac8059586cb95568675734a0784ba824dcdc15
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 17:56:00 +0900

    condition等のカテゴリーの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :category
-
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :shipping_date
 
   validates :name,             presence: true, length: { maximum: 40 }
   validates :description,      presence: true, length: { maximum: 1000 }
   validates :category_id,      presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :condition_id,     presence: true
-  validates :postage_id,       presence: true
+  validates :condition_id,     presence: true, numericality: { other_than: 0 , message: "can't be blank" }
+  validates :postage_id,       presence: true, numericality: { other_than: 0 , message: "can't be blank" }
   validates :prefecture_id,    presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :shipping_date_id, presence: true
+  validates :shipping_date_id, presence: true, numericality: { other_than: 0 , message: "can't be blank" }
   validates :price,            presence: true
 end
