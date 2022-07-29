db4d481abc778043e4ff6326a7bf858b769b8fa0
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 18:07:22 +0900

    priceのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,5 +15,7 @@ class Item < ApplicationRecord
   validates :postage_id,       presence: true, numericality: { other_than: 0 , message: "can't be blank" }
   validates :prefecture_id,    presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
   validates :shipping_date_id, presence: true, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :price,            presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equel_to: 9_999_999 , message: "can't be blank" }
+  end
 end
