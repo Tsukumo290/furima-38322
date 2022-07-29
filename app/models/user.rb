c5f5bbdadd9734dad2c23de6138fe3c48a5407d1
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 14:15:07 +0900

    名前(全角)のバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid. Input full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' } do
     validates :last_name
     validates :first_name
   end
