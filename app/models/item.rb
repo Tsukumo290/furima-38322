11624e9844d00c3e8dd6c8d87e1a958134c718e9
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 17:19:44 +0900

    prefectureカテゴリーの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 
   validates :name,             presence: true, length: { maximum: 40 }
   validates :description,      presence: true, length: { maximum: 1000 }
   validates :category_id,      presence: true
   validates :condition_id,     presence: true
   validates :postage_id,       presence: true
-  validates :prefecture_id,    presence: true
+  validates :prefecture_id,    presence: true, numericality: { other_than: 0 , message: "can't be blank" } 
   validates :shipping_date_id, presence: true
   validates :price,            presence: true
 end
