a3d3a05c4cec2e46db905d9464ab6d0a83e3c6a6
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 15:45:54 +0900

    imagesテーブルの作成、パラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,15 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
