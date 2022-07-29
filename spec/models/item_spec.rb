be270be841782761a49c6a10c3e702e823a26ce9
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-29 21:36:40 +0900

    アソシエーションの確認テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -90,6 +90,15 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Description is too long (maximum is 1000 characters)')
       end
+
+      #アソシエーションの確認
+
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    
     end
   end
 end
