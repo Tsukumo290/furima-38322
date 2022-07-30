c52be82c18f8355d404db19c6729ea2408f94cc0
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-30 21:53:01 +0900

    ルーティングの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,2 @@
 class OrdersController < ApplicationController
-  def index
-    @item = Item.find(params[:item_id])
-  end
 end
--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,2 +1,22 @@
 class PurchaseHistoriesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+   @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :prefecture_id, :municipality, :address, :building_name,
+                                           :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
 end
