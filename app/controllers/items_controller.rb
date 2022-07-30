97eeeb7d2153ef9ed63f0e97863de5846b723a13
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-30 19:13:38 +0900

    before_actionの順序修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :user_judge, only: [:edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
+  before_action :user_judge, only: [:edit, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -46,12 +46,12 @@ class ItemsController < ApplicationController
                                  :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
   def user_judge
     redirect_to root_path unless current_user.id == @item.user.id
   end
 
-  def item_find
-    @item = Item.find(params[:id])
-  end
-  
 end
