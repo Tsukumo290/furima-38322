3876679b8d2ef51582fd871c9af491b293ab771a
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-31 17:25:39 +0900

    バリデーション追加＆エラーメッセージの表示

--- app/models/purchase_history_order.rb
+++ app/models/purchase_history_order.rb
@@ -3,14 +3,15 @@ class PurchaseHistoryOrder
   attr_accessor :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "code is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
-    validates :token
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "number is too short" }
+    validates :phone_number, format: {with: /\A[0-9]+\z/, message: "number is invalid. Input only number" }
   end
 
   def save
