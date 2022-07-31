f01e3f1f0063fb9247195bb36fb28579c2df9775
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-08-01 01:05:43 +0900

    スペルミスの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   validates :shipping_date_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
   with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } do
     validates :price,
-              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equel_to: 9_999_999,
+              numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: 'out of setting range' }
   end
 end
