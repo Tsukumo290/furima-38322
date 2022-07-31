7e4c2a7f195e3ba1586cbf7d6c60153c710c9654
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-31 20:29:23 +0900

    idの修正

--- spec/factories/purchase_history_orders.rb
+++ spec/factories/purchase_history_orders.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     address                 { '知床' }
     phone_number            { '09012345678' }
     token                   { Faker::Internet.password(min_length: 20, max_length: 30) }
-    user_id                 { Faker::Number.non_zero_digit }
-    item_id                 { Faker::Number.non_zero_digit }
+    user_id                 { 1 }
+    item_id                 { 1 }
   end
 end
